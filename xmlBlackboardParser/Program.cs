e1f40a188e189e3eeba5813ec8fd02f9b551ba24
faridskyman <farid.skyman@gmail.com>
2015-06-18 12:47:55 +0800

    has some basic xml parsin working

--- xmlBlackboardParser/Program.cs
+++ xmlBlackboardParser/Program.cs
@@ -10,26 +10,96 @@ namespace xmlBlackboardParser
 {
     class Program
     {
+
+
+
+
         static void Main(string[] args)
         {
-            parseXML();
+            //parseXML();
+            parseXML2();
         }
 
+        private static void parseXML2()
+        {
+
+                    string xmldoc =
+    @"<?xml version='1.0'?>
+<catalog>
+  <book id='bk101'>
+    <author>Steve Jin</author>
+    <title>VMware VI and vSphere SDK</title>
+    <genre>Computer</genre>
+    <price>59.99</price>
+    <publish_date>2009-09-01</publish_date>
+    <description>
+      An in-depth introduction on VMware vSphere SDK and open source vijava API.
+    </description>
+  </book>
+  <book id='bk102'>
+    <author>Steve Jin</author>
+    <title>TBD</title>
+    <genre>Computer</genre>
+    <price>59.99</price>
+    <publish_date>2015-09-01</publish_date>
+    <description>
+      An in-depth introduction on something.
+    </description>
+ </book>
+</catalog>";
+
+            XmlDocument doc = new XmlDocument();
+            //doc.Load(@"c:\users\steve\documents\books.xml");
+            doc.LoadXml(xmldoc);
+
+
+
+ 
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("/catalog/book");
+ 
+            List<Book> books = new List<Book>();
+ 
+            foreach (XmlNode node in nodes)
+            {
+                Book book = new Book();
+ 
+                book.author = node.SelectSingleNode("author").InnerText;
+                book.title = node.SelectSingleNode("title").InnerText;
+                book.id = node.Attributes["id"].Value;
+ 
+                books.Add(book);
+            }
+ 
+            System.Console.WriteLine("Total books: " + books.Count);
+            
+            foreach (Book b in books)
+            {
+                Console.WriteLine(b.author + " - " + b.title);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        
+
+        
+        
         private static void parseXML()
         {
             StringBuilder output = new StringBuilder();
 
             String xmlString =
                 @"<bookstore>
-        <book genre='autobiography' publicationdate='1981-03-22' ISBN='1-861003-11-0'>
-            <title>The Autobiography of Benjamin Franklin</title>
-            <author>
-                <first-name>Benjamin</first-name>
-                <last-name>Franklin</last-name>
-            </author>
-            <price>8.99</price>
-        </book>
-    </bookstore>";
+                    <book genre='autobiography' publicationdate='1981-03-22' ISBN='1-861003-11-0'>
+                        <title>The Autobiography of Benjamin Franklin</title>
+                        <author>
+                            <first-name>Benjamin</first-name>
+                            <last-name>Franklin</last-name>
+                        </author>
+                        <price>8.99</price>
+                    </book>
+                </bookstore>";
 
             // Create an XmlReader
             using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
@@ -47,4 +117,13 @@ namespace xmlBlackboardParser
             Console.ReadLine();
         }
     }
+
+
+    class Book
+    {
+        public string id;
+        public string title;
+        public string author;
+    }        
+        
 }
