814278639fb5a6062914d69d759e24dfbe1e3a3b
faridskyman <farid.skyman@gmail.com>
2015-06-18 12:24:53 +0800

    Added program

--- xmlBlackboardParser/Program.cs
+++ xmlBlackboardParser/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace xmlBlackboardParser
 {
@@ -10,6 +12,39 @@ namespace xmlBlackboardParser
     {
         static void Main(string[] args)
         {
+            parseXML();
+        }
+
+        private static void parseXML()
+        {
+            StringBuilder output = new StringBuilder();
+
+            String xmlString =
+                @"<bookstore>
+        <book genre='autobiography' publicationdate='1981-03-22' ISBN='1-861003-11-0'>
+            <title>The Autobiography of Benjamin Franklin</title>
+            <author>
+                <first-name>Benjamin</first-name>
+                <last-name>Franklin</last-name>
+            </author>
+            <price>8.99</price>
+        </book>
+    </bookstore>";
+
+            // Create an XmlReader
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+            {
+                reader.ReadToFollowing("book");
+                reader.MoveToFirstAttribute();
+                string genre = reader.Value;
+                output.AppendLine("The genre value: " + genre);
+
+                reader.ReadToFollowing("title");
+                output.AppendLine("Content of the title element: " + reader.ReadElementContentAsString());
+            }
+            
+            Console.WriteLine(output.ToString());
+            Console.ReadLine();
         }
     }
 }
