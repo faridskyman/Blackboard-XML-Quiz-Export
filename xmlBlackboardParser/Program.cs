d1f4d7dba82e0644e4202fa774fb6e5d07b03920
faridskyman <farid.skyman@gmail.com>
2015-06-19 08:32:10 +0800

    completed first working draft where Question type, question and options are extracted, altho option are hardcoded to only et the first 4 values and will throw err if option is less then 4. hence this works only for MCQ with 4 option. Correct response is not in yet.

--- xmlBlackboardParser/Program.cs
+++ xmlBlackboardParser/Program.cs
@@ -17,7 +17,75 @@ namespace xmlBlackboardParser
         static void Main(string[] args)
         {
             //parseXML();
-            parseXML2();
+//            parseXML2();
+            parseXML3();
+        }
+
+
+        private static void parseXML3()
+        {
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\Users\farid.ANL\Documents\visual studio 2013\Projects\xmlBlackboardParser\xmlBlackboardParser\doc2.xml");
+            //doc.LoadXml(xmldoc);
+
+
+
+
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("/questestinterop/assessment/section/item");
+
+            List<Book> books = new List<Book>();
+            List<questionItem> qItems = new List<questionItem>();
+
+            foreach (XmlNode node in nodes)
+            {
+                //Book book = new Book();
+                questionItem qitem = new questionItem();
+
+                XmlNodeList itemmetadata = node.SelectNodes("itemmetadata");
+                XmlNodeList presentation = node.SelectNodes("presentation/flow/flow");
+                XmlNodeList QUESTION_BLOCK = presentation.Item(0).ChildNodes;
+                XmlNodeList RESPONSE_BLOCK = presentation.Item(1).ChildNodes;
+
+                XmlNodeList QN = QUESTION_BLOCK.Item(0).SelectNodes("material/mat_extension");
+                XmlNodeList RES = RESPONSE_BLOCK.Item(0).SelectNodes("render_choice/flow_label "); //all choices are  in here
+
+                XmlNodeList RES1 = RES.Item(0).SelectNodes("response_label/flow_mat/material/mat_extension");
+
+                    
+                    //n1.Item(0).SelectSingleNode
+                //nodeList = root.SelectNodes("/bookstore/book/@bk:ISBN", nsmgr);
+
+                qitem.questiontype = itemmetadata.Item(0).SelectSingleNode("bbmd_questiontype").InnerText;
+                qitem.question = QN.Item(0).SelectSingleNode("mat_formattedtext").InnerText;
+                qitem.option1 = RES.Item(0).SelectSingleNode("response_label/flow_mat/material/mat_extension").InnerText;
+                qitem.option2 = RES.Item(1).SelectSingleNode("response_label/flow_mat/material/mat_extension").InnerText;
+                qitem.option3 = RES.Item(2).SelectSingleNode("response_label/flow_mat/material/mat_extension").InnerText;
+                qitem.option4 = RES.Item(3).SelectSingleNode("response_label/flow_mat/material/mat_extension").InnerText;
+                //book.author = node.SelectSingleNode("author").InnerText;
+                //book.title = node.SelectSingleNode("title").InnerText;
+                //book.id = node.Attributes["id"].Value;
+
+                //books.Add(book);
+                qItems.Add(qitem);
+            }
+
+            //System.Console.WriteLine("Total books: " + books.Count);
+
+            foreach (questionItem a in qItems)
+            {
+                Console.WriteLine(a.questiontype
+                    + Environment.NewLine + "Q:" + a.question 
+                    + Environment.NewLine + "a." + a.option1
+                    + Environment.NewLine + "b." + a.option2
+                    + Environment.NewLine + "c." + a.option3
+                    + Environment.NewLine + "d." + a.option4
+                    );
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
 
         private static void parseXML2()
@@ -100,6 +168,19 @@ namespace xmlBlackboardParser
         public string id;
         public string title;
         public string author;
-    }        
+    }
+
+    class questionItem
+    {
+        public string questiontype;
+        public string question;
+        public string option1;
+        public string option2;
+        public string option3;
+        public string option4;
+        public string answer;
+    }
+
+
         
 }
